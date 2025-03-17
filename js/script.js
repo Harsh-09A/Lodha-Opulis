12ef64bbd0fe7f9008bcd5708fde4ef99fad8601
Harsh-09A <harsh.autowebbed@gmail.com>
2025-03-17 13:48:43 +0530

    Sir Made Changes

--- js/script.js
+++ js/script.js
@@ -14,3 +14,27 @@ function toggleContent(dotsId, moreTextId, btnId) {
     moreText.style.display = "inline";
   }
 }
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  autoplay: true,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 4,
+    },
+  },
+});
+
+$(window).ready(function () {
+  setTimeout(function () {
+    $("#popupModal").modal("show");
+  }, 3000);
+});
